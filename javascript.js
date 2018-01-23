e783327ffc13873fe2785894560bb2c78a0b2f14
DanteMaxF <A01207543@itesm.mx>
2018-01-23 00:09:14 -0600

    Agregar mas ejercicios [Falta el ultimo]

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function homeButtonDisplay(){
 
 
 function ejercicio1(){
-  var input = prompt("test");
+  var input = prompt("Ingresa un número entero");
   input = parseInt(input);
   for(var n = 1; n <= input; n++){
     document.writeln(n+" - ");
@@ -65,8 +65,43 @@ function contador(){
   homeButtonDisplay();
 }
 
+/*Se que no es el mejor algoritmo para crear una matriz aleatoria,
+pero es lo primero que se me vino a la mente*/
 function promedios(){
+  var matriz = [];
+  var submatriz1 = [];
+  var submatriz2 = [];
+  var submatriz3 = [];
+  for(var n=0; n < 10; n++){
+    for(var m=0; m < 10; m++){
+        submatriz1.push(Math.floor((Math.random() * 50) + 1));
+        submatriz2.push(Math.floor((Math.random() * 50) + 1));
+        submatriz3.push(Math.floor((Math.random() * 50) + 1));
+    }
+    matriz.push(submatriz1);
+    matriz.push(submatriz2);
+    matriz.push(submatriz3);
+  }
+  var sum = 0;
+  for(var n=0; n < 10; n++){
+    for(var m=0; m < 10; m++){
+      document.writeln(" | "+matriz[n][m]);
+      sum += matriz[n][m];
+    }
+    document.writeln("|  PROMEDIO: "+sum/10);
+    sum = 0;
+    document.writeln("<br>");
+  }
+  homeButtonDisplay();
+}
 
+function inverso(){
+  var input = prompt("Ingresa un número entero");
+  inputLength = input.length-1;
+  for(; inputLength >= 0; inputLength--){
+    console.log(input[inputLength]);
+    document.write(input[inputLength]);
+  }
 
   homeButtonDisplay();
 }
